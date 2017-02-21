de52ebcef5e9935846db989432b9a5d42f98453e
Yusaku Maezono <yusakumaezono@gmail.com>
2017-02-21 18:31:58 +0900

    change package name

--- KongHmac/KongHmac.php
+++ KongHmac/KongHmac.php
@@ -1,21 +1,13 @@
 <?php
 namespace KongHmac;
 
-use Monolog\Logger;
-use Monolog\Handler\StreamHandler;
-
 class KongHmac
 {
-    public $log;
-
-    public function __construct($path = './hello.log')
-    {
-        $this->log = new Logger('HelloWorld');
-        $this->log->pushHandler(new StreamHandler($path, Logger::INFO));
-    }
+    public function __construct()
+    {}
 
     public function hello($name = 'World')
     {
-        $this->log->addInfo('Hello '.$name);
+        echo $name;
     }
 }
